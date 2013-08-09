24b9f13988cbe370401827c1ef3528b9863b6af3
Laurent Cottereau <laurent@cottereau.name>
2013-08-09 22:48:31 +0200

    mise en place initiale du site

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="fr"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang="fr"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang="fr"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="fr"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Laurent Cottereau &ndash; Clarky's Corner</title>
+        <meta name="description" content="Page personnelle de Laurent Cottereau">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="/css/normalize.min.css">
+        <link rel="stylesheet" href="/css/main.css">
+        <link rel="stylesheet" href="/css/pygments.css">
+
+        <link rel="openid.server" href="http://openid.claimid.com/server" />
+        <link rel="openid.delegate" href="http://openid.claimid.com/lcottereau" />
+
+        <link rel="pavatar" href="/img/avatar/09.jpg" />
+
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div id="main-container">
+            <div id="main" class="wrapper clearfix">
+                <header>
+                    <h1>Laurent Cottereau</h1>
+                </header>
+
+                <article>
+                  <p>
+                    Je suis un <a href="http://www.linkedin.com/in/lcottereau/fr" title="CV en ligne de Laurent Cottereau">artisan du logiciel web et agile</a>.
+                    Je m'intéresse à tous les aspects techniques d'une application web. J'ai par ailleurs une expertise particulière en J2EE, CSS, accessibilité web
+                    et sur les outils ou techniques aidant à l'industrialisation de la
+                    <a href="https://fr.wikipedia.org/wiki/Software_craftsmanship" title="L'artisanat du logiciel sur Wikipedia">qualité applicative</a>.
+                  </p>
+
+                  <p>
+                    Quand je ne suis pas derrière un écran, je passe beaucoup de temps sur les terrains de rugby du
+                    <a href="http://scufrugby.over-blog.com/"><acronym title="Sporting Club Universitaire de France">SCUF</acronym></a> ou du
+		                <a href="http://www.xvparlementaire.fr">XV Parlementaire</a>, ou encore sur
+                    <a hreflang="en" href="http://www.shareyouradventure.com/user/profile/lcottereau">des chemins de randonnée</a>. J'essaie (avec plus ou moins de réussite)
+                    de trouver le temps de jouer un peu de mon <a href="http://www.association-irlandaise.org/">tin-whistle</a> ou d'écrire un peu
+		                dans <a href="/blog">mon blog</a>.
+                  </p>
+
+                  <p>
+                    Vous pouvez me contacter par mail ou jabber sur <code><a href="mailto:laurent@cottereau.name">laurent@cottereau.name</a></code>.
+                    N'hésitez pas à utiliser ma clé <a hreflang="en" href="http://www.gnupg.org/">GnuPG</a>
+                    [<a href="/lcottereau_key.asc" title="Clé GnuPG de Laurent Cottereau">1024D/F616F688</a>@<a hreflang="en" 
+                    href="http://pgp.mit.edu:11371/pks/lookup?op=get&search=0x52E64D41F616F688" title="Clé GnuPG (signée) de Laurent Cottereau sur le serveur du MIT">pgp.mit.edu</a>].
+                    Vous pouvez aussi me suivre sur les réseaux sociaux.
+                  </p>
+                </article>
+
+
+                <nav>
+                    <ul>
+                        <li><a href="http://www.linkedin.com/in/lcottereau/fr" title="Mon CV sur LinkedIn"><img src="img/link-linkedin.png" alt="CV"/></a></li>
+                        <li><a hreflang="en" href="https://github.com/lcottereau" title="Mon code sur GitHub"><img src="img/link-github.png" alt="Développement"/></a></li>
+                        <li><a href="http://laurent.cottereau.name/blog/" title="Mes écrits sur Dotclear"><img src="img/link-blog.png" alt="Blog"/></a></li>
+                        <li><a href="https://twitter.com/lcottereau" title="Mes divagations sur Twitter"><img src="img/link-twitter.png" alt="Microblog"/></a></li>
+                        <li><a href="http://500px.com/lcottereau" title="Une sélection de mes photos sur 500px"><img src="img/link-photos.png" alt="Photoblog"/></a></li>
+                        <li><a hreflang="en" href="http://www.shareyouradventure.com/user/profile/lcottereau" title="Mes randonnées sur Social Hiking"><img src="img/link-hiking.png" alt="Randonnées"/></a></li>
+                        <li><a hreflang="en" href="https://www.librarything.com/profile/lcottereau" title="Mes lectures sur LibraryThing"><img src="img/link-books.png" alt="Lectures"/></a></li>
+                        <li><a href="http://www.lastfm.fr/user/lcottereau" title="La musique que j'écoute sur Lastfm"><img src="img/link-music.png" alt="Musique"/></a></li>
+                    </ul>
+                </nav>
+
+
+            </div> <!-- #main -->
+        </div> <!-- #main-container -->
+
+        <div class="footer-container">
+            <footer>
+
+
+              <p id="copyleft">
+                Sauf <a href="https://github.com/lcottereau/name.cottereau.laurent#crdits">mention contraire</a>,
+                le contenu de ce site est la propriété de <a href="mailto:laurent@cottereau.name" accesskey="7">Laurent Cottereau</a>
+                et est mis &agrave; disposition sous
+                <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/2.0/fr/">contrat Creative Commons BY-NC-SA</a>.
+                Les documents cités ou liés peuvent être soumis au droit d'auteur.
+                <!-- /Contrat Creative Commons -->
+
+
+                <!--
+                <rdf:RDF xmlns="http://web.resource.org/cc/"
+                      xmlns:dc="http://purl.org/dc/elements/1.1/"
+                      xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+                  <Work rdf:about="">
+                     <dc:title>Clarky's Corner</dc:title>
+                     <dc:date>2005</dc:date>
+                     <dc:creator>
+                      <Agent>
+                         <dc:title>Laurent Cottereau</dc:title>
+                      </Agent>
+                     </dc:creator>
+                     <license rdf:resource="http://creativecommons.org/licenses/by-nc-sa/2.0/fr/" />
+                  </Work>
+
+                  <License rdf:about="http://creativecommons.org/licenses/by-nc-sa/2.0/fr/">
+                     <permits rdf:resource="http://web.resource.org/cc/Reproduction" />
+                     <permits rdf:resource="http://web.resource.org/cc/Distribution" />
+                     <requires rdf:resource="http://web.resource.org/cc/Notice" />
+                     <requires rdf:resource="http://web.resource.org/cc/Attribution" />
+                     <prohibits rdf:resource="http://web.resource.org/cc/CommercialUse" />
+                     <permits rdf:resource="http://web.resource.org/cc/DerivativeWorks" />
+                     <requires rdf:resource="http://web.resource.org/cc/ShareAlike" />
+                  </License>
+
+                </rdf:RDF>
+
+                -->
+
+
+
+
+            </footer>
+        </div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+
+        <script src="js/main.js"></script>
+    </body>
+</html>
+
